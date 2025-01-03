6bef998d031f084b9a58ad7587cb0353c138c848
Goran Cvijanovic <goran.rs.bg@gmail.com>
2025-01-03 19:59:06 +0100

    add green for selected tab border

--- config.def.h
+++ config.def.h
@@ -16,12 +16,13 @@ static const char col_gray2[]       = "#282a36";
 static const char col_gray3[]       = "#96b5b4";
 static const char col_gray4[]       = "#d7d7d7";
 static const char col_cyan[]        = "#4e4e4e";
+static const char col_green[]        = "#15B31D";
 static const unsigned int baralpha  = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_green  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border     */
